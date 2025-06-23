71de02452a836c664180c6fa4432cd07c6d54053
Mark Kim <mkim797@yahoo.com>
2025-06-23 13:15:37 -0700

    add else statements to show elements not removed

--- rm_msautoupdate.sh
+++ rm_msautoupdate.sh
@@ -2,20 +2,28 @@
 
 if [ -d /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app ]; then
     rm -rf /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app
-    echo "REMOVED Microsoft AutoUpdate.app"
+    echo "REMOVED Microsoft AutoUpdate.app"; then
+else
+    echo "Microsoft AutoUpdate.app NOT REMOVED; not found"
 fi
 
 if [ -f /Library/LaunchAgents/com.microsoft.update.agent.plist ]; then
     rm -f /Library/LaunchAgents/com.microsoft.update.agent.plist
-    echo "REMOVED com.microsoft.update.agent.plist"
+    echo "REMOVED com.microsoft.update.agent.plist"; then
+else
+    echo "com.microsoft.update.agent.plist NOT REMOVED; not found"
 fi
 
 if [ -f /Library/LaunchDaemons/com.microsoft.autoupdate.helper.plist ]; then
     rm -f /Library/LaunchDaemons/com.microsoft.autoupdate.helper.plist
-    echo "REMOVED com.microsoft.autoupdate.helper.plist from LaunchDaemons"
+    echo "REMOVED com.microsoft.autoupdate.helper.plist from LaunchDaemons"; then
+else
+    echo "com.microsoft.autoupdate.helper.plist NOT REMOVED; not found"
 fi
 
 if [ -f /Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper.plist ]; then
     rm -f /Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper.plist
-    echo "REMOVED com.microsoft.autoupdate.helper.plist from PrivilegedHelperTools"
+    echo "REMOVED com.microsoft.autoupdate.helper.plist from PrivilegedHelperTools"; then
+else
+    echo "com.microsoft.autoupdate.helper.plist NOT REMOVED; not found"
 fi
